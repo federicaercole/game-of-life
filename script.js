abd5e4eed4fcbd9eeabe28884d7aea5bf7427b12
Federica Ercole <126322565+federicaercole@users.noreply.github.com>
2023-05-27 12:14:51 +0200

    Add function to create board

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ export const toggleState = (cell) => {
         alive: !cell.alive
     }
 };
+
+const gameboardOptions = Object.freeze({
+    rows: 10,
+    colums: 10
+});
+
+export const createGameBoard = (options) => {
+    let gameboard = [];
+
+    for (let y = 0; y < options.rows; y++) {
+        for (let x = 0; x < options.colums; x++) {
+            gameboard.push(cell(false, x, y));
+        }
+    }
+    return gameboard;
+};
--- tests/script.test.js
+++ tests/script.test.js
@@ -1,4 +1,4 @@
-import { cell, toggleState } from "../script";
+import { cell, toggleState, createGameBoard } from "../script";
 
 describe("cell creation", () => {
 
@@ -16,5 +16,36 @@ describe("cell creation", () => {
     test("it changes alive state another time", () => {
         const becomeDeadCell = toggleState(aliveCell);
         expect(becomeDeadCell).toEqual({ alive: false, x: 0, y: 0 });
+    });
+});
+
+describe("gameboard", () => {
+
+    const gameboardOptions = Object.freeze({
+        rows: 10,
+        colums: 10
+    });
+
+    const gameboard = createGameBoard(gameboardOptions);
+
+    test("it creates gameboard", () => {
+        expect(gameboard.length).toEqual(100);
+    });
+
+    test("last element", () => {
+        expect(gameboard[99]).toEqual({ alive: false, x: 9, y: 9 });
+    });
+
+    test("get neighbors of cell x: 1, y:1", () => {
+        expect(gameboard[11]).toEqual({ alive: false, x: 1, y: 1 });
+
+        expect(gameboard[0]).toEqual({ alive: false, x: 0, y: 0 });
+        expect(gameboard[1]).toEqual({ alive: false, x: 1, y: 0 });
+        expect(gameboard[2]).toEqual({ alive: false, x: 2, y: 0 });
+        expect(gameboard[10]).toEqual({ alive: false, x: 0, y: 1 });
+        expect(gameboard[12]).toEqual({ alive: false, x: 2, y: 1 });
+        expect(gameboard[20]).toEqual({ alive: false, x: 0, y: 2 });
+        expect(gameboard[21]).toEqual({ alive: false, x: 1, y: 2 });
+        expect(gameboard[22]).toEqual({ alive: false, x: 2, y: 2 });
     });
 });
