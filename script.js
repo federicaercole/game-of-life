d7821cdcc2ac853d6107444b4318af1cc696e79e
Federica Ercole <126322565+federicaercole@users.noreply.github.com>
2023-05-28 10:12:01 +0200

    Start game logic functions

--- script.js
+++ script.js
@@ -12,31 +12,51 @@ export const changeAliveState = (cell, value) => {
         ...cell,
         alive: value
     }
-}
+};
 
 const gameboardOptions = {
     rows: 10,
-    colums: 10,
+    columns: 10,
 };
 
 export const createGameBoard = (options) => {
     let gameboard = [];
 
     for (let y = 0; y < options.rows; y++) {
-        for (let x = 0; x < options.colums; x++) {
+        for (let x = 0; x < options.columns; x++) {
             gameboard.push(cell(x, y));
         }
     }
     return gameboard;
 };
 
+export const gameboard = createGameBoard(gameboardOptions);
+
 export const getCellIndex = (x, y) => {
-    return x + (y * gameboardOptions.colums);
+    return x + (y * gameboardOptions.columns);
 };
 
-export const randomizeBoard = (options) => {
-    const randomBoard = createGameBoard(options).map(cell => {
+export const randomizeBoard = () => {
+    const randomBoard = gameboard.map(cell => {
         return changeAliveState(cell, Math.random() > 0.5);
     });
     return randomBoard;
+};
+
+export const checkNeighbors = (cell) => {
+    const x = cell.x;
+    const y = cell.y;
+
+    let numberOfAliveCells = checkAliveState(x - 1, y - 1) + checkAliveState(x, y - 1) + checkAliveState(x + 1, y - 1) +
+        checkAliveState(x - 1, y) + checkAliveState(x + 1, y) + checkAliveState(x - 1, y + 1) + checkAliveState(x, y + 1) + checkAliveState(x + 1, y + 1);
+
+    return numberOfAliveCells;
+};
+
+const checkAliveState = (x, y) => {
+    const cellIndex = getCellIndex(x, y);
+    if (x < 0 || y < 0 || x >= gameboardOptions.columns || y >= gameboardOptions.rows) {
+        return 0;
+    }
+    return gameboard[cellIndex].alive ? 1 : 0;
 };
--- tests/script.test.js
+++ tests/script.test.js
@@ -1,4 +1,4 @@
-import { cell, changeAliveState, createGameBoard, getCellIndex, randomizeBoard } from "../script";
+import { cell, gameboard, changeAliveState, createGameBoard, getCellIndex, randomizeBoard, checkNeighbors } from "../script";
 
 describe("cell creation", () => {
 
@@ -21,44 +21,66 @@ describe("cell creation", () => {
 
 describe("gameboard", () => {
 
-    const gameboardOptions = Object.freeze({
+    const gameboardOptions = {
         rows: 10,
-        colums: 10,
-    });
+        columns: 10,
+    };
 
-    const gameboard = createGameBoard(gameboardOptions);
+    const gameboardTest = createGameBoard(gameboardOptions);
 
     test("it creates gameboard", () => {
-        expect(gameboard.length).toEqual(100);
+        expect(gameboardTest.length).toEqual(100);
     });
 
     test("last element", () => {
-        expect(gameboard[99]).toEqual({ alive: false, x: 9, y: 9 });
+        expect(gameboardTest[99]).toEqual({ alive: false, x: 9, y: 9 });
     });
 
     test("get neighbors of cell x: 1, y:1", () => {
-        expect(gameboard[11]).toEqual({ alive: false, x: 1, y: 1 });
+        expect(gameboardTest[11]).toEqual({ alive: false, x: 1, y: 1 });
 
-        expect(gameboard[0]).toEqual({ alive: false, x: 0, y: 0 });
-        expect(gameboard[1]).toEqual({ alive: false, x: 1, y: 0 });
-        expect(gameboard[2]).toEqual({ alive: false, x: 2, y: 0 });
-        expect(gameboard[10]).toEqual({ alive: false, x: 0, y: 1 });
-        expect(gameboard[12]).toEqual({ alive: false, x: 2, y: 1 });
-        expect(gameboard[20]).toEqual({ alive: false, x: 0, y: 2 });
-        expect(gameboard[21]).toEqual({ alive: false, x: 1, y: 2 });
-        expect(gameboard[22]).toEqual({ alive: false, x: 2, y: 2 });
+        expect(gameboardTest[0]).toEqual({ alive: false, x: 0, y: 0 });
+        expect(gameboardTest[1]).toEqual({ alive: false, x: 1, y: 0 });
+        expect(gameboardTest[2]).toEqual({ alive: false, x: 2, y: 0 });
+        expect(gameboardTest[10]).toEqual({ alive: false, x: 0, y: 1 });
+        expect(gameboardTest[12]).toEqual({ alive: false, x: 2, y: 1 });
+        expect(gameboardTest[20]).toEqual({ alive: false, x: 0, y: 2 });
+        expect(gameboardTest[21]).toEqual({ alive: false, x: 1, y: 2 });
+        expect(gameboardTest[22]).toEqual({ alive: false, x: 2, y: 2 });
     });
 
     test("get the status of some cells", () => {
         const indexCell1 = getCellIndex(0, 1);
         const indexCell2 = getCellIndex(1, 1);
 
-        expect(gameboard[indexCell1]).toEqual({ alive: false, x: 0, y: 1 });
-        expect(gameboard[indexCell2]).toEqual({ alive: false, x: 1, y: 1 });
+        expect(gameboardTest[indexCell1]).toEqual({ alive: false, x: 0, y: 1 });
+        expect(gameboardTest[indexCell2]).toEqual({ alive: false, x: 1, y: 1 });
     });
 
     test("create a board with random cells alive state", () => {
-        const randomBoard = randomizeBoard(gameboardOptions);
+        const randomBoard = randomizeBoard();
         expect(randomBoard.some(cell => cell.alive === true)).toBeTruthy();
     })
 });
+
+describe("game logic", () => {
+
+    test("check number of alive cells around cell x: 1, y:1", () => {
+        gameboard[0].alive = true;
+        gameboard[1].alive = true;
+        gameboard[10].alive = true;
+
+        const numberAlive = checkNeighbors(gameboard[11]);
+
+        expect(numberAlive).toBe(3);
+    });
+
+    test("check number of alive cells around cell x:0, y:0", () => {
+        gameboard[1].alive = true;
+        gameboard[10].alive = true;
+
+        const numberAlive = checkNeighbors(gameboard[0]);
+
+        expect(numberAlive).toBe(2);
+    });
+})
